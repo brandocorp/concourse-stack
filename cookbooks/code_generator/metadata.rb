ce6dfe2698749377b758a365df0609e2027dae56
Brandon Raabe <brandocorp@gmail.com>
2017-08-01 21:33:36 +0000

    Update default recipe spec template

--- cookbooks/code_generator/metadata.rb
+++ cookbooks/code_generator/metadata.rb
@@ -2,4 +2,3 @@ name             'code_generator'
 description      'Custom code generator cookbook for use with ChefDK'
 long_description 'Custom code generator cookbook for use with ChefDK'
 version          '0.1.0'
-
